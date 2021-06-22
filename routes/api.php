31eff5d1842048ff1e32b8aeed021a2b565cf948
Pedja1990 <predragmiljus11@gmail.com>
2021-06-23 00:01:26 +0200

    authController added, routes for login, register, logout, jwt token added

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +21,16 @@ use App\Http\Controllers\GalleryController;
 //     return $request->user();
 // });
 
+
 Route::middleware('api')->get('/galleries', [GalleryController::class,'index']);
 Route::middleware('api')->get('/galleries/{id}',[GalleryController::class,'show']);
 Route::middleware('api')->post('/galleries', [GalleryController::class,'store']);
 Route::middleware('api')->put('/galleries/{id}', [GalleryController::class,'update']);
 Route::middleware('api')->delete('/galleries/{id}', [GalleryController::class,'destroy']);
+
+Route::post('register', [ AuthController::class, 'register' ])->middleware('guest:api');
+Route::post('login', [ AuthController::class, 'login' ])->middleware('guest:api');
+Route::post('logout', [ AuthController::class, 'logout' ])->middleware('auth:api');
+Route::get('me', [ AuthController::class, 'me' ])->middleware('auth:api');
 
 
